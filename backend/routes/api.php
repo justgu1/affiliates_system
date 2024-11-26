e9d0782c28d4088903086931a99c20f7504041f3
just Gui <tkzebrola@gmail.com>
2024-11-26 14:14:36 -0300

    Complete AuthController with sanctum API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('user')->name('user.')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+    Route::get('verify', [AuthController::class, 'verify'])->name('verify')->middleware('auth:sanctum');
+});
